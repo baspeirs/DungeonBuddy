02c4aaea8ba75143b68b4a481436d6ffeec91f43
Bryce Speirs <baspeirs@gmail.com>
2020-06-15 12:15:48 -0400

    updtated dice roll page to roll the dice when clicking the image and changed main font for all pages

--- assets/dice.js
+++ assets/dice.js
@@ -26,3 +26,44 @@ $("#rollButton").on("click", function() {
     $("#rollValue").append(valueDiv)
 })
 
+function diceRoll(num) {
+    $("#rollValue").empty();
+    let roll = Math.floor(Math.random() * num + 1);
+    $("#rollValue").text(roll);
+    console.log(roll)
+}
+
+$("#img-dice-4").on("click", () => {
+    console.log("I am being clicked! 4")
+    diceRoll(4)
+});
+
+$("#img-dice-6").on("click", () => {
+    console.log("I am being clicked! 6")
+    diceRoll(6)
+});
+
+$("#img-dice-8").on("click", () => {
+    console.log("I am being clicked! 8")
+    diceRoll(8)
+});
+
+$("#img-dice-10").on("click", () => {
+    console.log("I am being clicked! 10")
+    diceRoll(10)
+});
+
+$("#img-dice-12").on("click", () => {
+    console.log("I am being clicked! 12")
+    diceRoll(12)
+});
+
+$("#img-dice-20").on("click", () => {
+    console.log("I am being clicked! 20")
+    diceRoll(20)
+});
+
+$("#img-dice-percent").on("click", () => {
+    console.log("I am being clicked! percent")
+    diceRoll(100)
+});
